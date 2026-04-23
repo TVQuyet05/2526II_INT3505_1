4b61a9e0f388403bee3a77f0cf55b4c018577051
TVQuyet05 <tranvanquyet09112005@gmail.com>
2026-04-23 21:26:05 +0700

    feat(Week9): code HTTP Headers for Deprecation

--- Week9/Code_Demo/v1.py
+++ Week9/Code_Demo/v1.py
@@ -12,9 +12,16 @@ def process_payment():
     amount = data.get('amount')
     
     # Version 1 defaults to VND
-    return jsonify({
-        "message": "Payment processed successfully",
+    response = jsonify({
+        "message": "Payment processed successfully (DEPRECATED)",
         "amount": amount,
         "currency": "VND",
         "version": "v1"
-    }), 200
+    })
+    
+    # Adding Deprecation HTTP Headers
+    response.headers['Deprecation'] = 'true'  # Đánh dấu API đã deprecated
+    response.headers['Sunset'] = 'Thu, 31 Dec 2026 23:59:59 GMT' # Ngày khai tử chính thức
+    response.headers['Link'] = '<http://localhost:5000/api/v2/payment>; rel="alternate"' # Link chuyển hướng sang v2
+    
+    return response, 200
